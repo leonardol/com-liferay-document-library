marcellustavares/liferay-portal

LPS-69400 update references for Document Library

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/asset/folder_full_content.jsp
@@ -67,14 +67,14 @@ Folder folder = (Folder)request.getAttribute(WebKeys.DOCUMENT_LIBRARY_FOLDER);
 				</div>
 			</div>
 
-			<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= DLFolderConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= DLFolderConstants.getClassName() %>"
 					classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 		</aui:col>
 	</aui:row>
 </c:if>

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/edit_file_entry.jsp
@@ -450,16 +450,16 @@ if (portletTitleBasedNavigation) {
 				</c:if>
 
 				<c:if test="<%= (folder == null) || folder.isSupportsMetadata() %>">
-					<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
+					<liferay-custom-field-ui:custom-attribute-available className="<%= DLFileEntryConstants.getClassName() %>">
 						<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-							<liferay-ui:custom-attribute-list
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= DLFileEntryConstants.getClassName() %>"
 								classPK="<%= fileVersionId %>"
 								editable="<%= true %>"
 								label="<%= true %>"
 							/>
 						</aui:fieldset>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attribute-available>
 				</c:if>
 
 				<c:if test="<%= (folder == null) || folder.isSupportsSocial() %>">

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/edit_folder.jsp
@@ -287,16 +287,16 @@ if (portletTitleBasedNavigation) {
 			</c:if>
 
 			<c:if test="<%= (parentFolder == null) || parentFolder.isSupportsMetadata() %>">
-				<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attribute-available className="<%= DLFolderConstants.getClassName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFolderConstants.getClassName() %>"
 							classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attribute-available>
 			</c:if>
 
 			<c:if test="<%= !rootFolder && (folder == null) %>">

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/info_panel.jsp
@@ -248,14 +248,14 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(fileEntries) && ListUtil.isEmp
 						classPK="<%= fileEntry.getFileEntryId() %>"
 					/>
 
-					<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-available className="<%= DLFileEntryConstants.getClassName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFileEntryConstants.getClassName() %>"
 							classPK="<%= fileVersion.getFileVersionId() %>"
 							editable="<%= false %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attribute-available>
 
 					<%
 					AssetEntry layoutAssetEntry = AssetEntryLocalServiceUtil.fetchEntry(DLFileEntryConstants.getClassName(), assetClassPK);

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/upload_multiple_file_entries_resources.jsp
@@ -219,14 +219,14 @@ else {
 				</liferay-ui:panel>
 			</c:if>
 
-			<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= DLFileEntryConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= DLFileEntryConstants.getClassName() %>"
 					classPK="<%= fileVersionId %>"
 					editable="<%= true %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 		</c:if>
 
 		<c:if test="<%= (folder == null) || folder.isSupportsSocial() %>">

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/view_file_entry.jsp
@@ -320,16 +320,16 @@ if (portletTitleBasedNavigation) {
 
 						</c:if>
 
-						<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>" classPK="<%= fileVersionId %>" editable="<%= false %>">
+						<liferay-custom-field-ui:custom-attribute-available className="<%= DLFileEntryConstants.getClassName() %>" classPK="<%= fileVersionId %>" editable="<%= false %>">
 							<liferay-ui:panel collapsible="<%= true %>" cssClass="lfr-custom-fields" id="documentLibraryCustomFieldsPanel" markupView="lexicon" persistState="<%= true %>" title="custom-fields">
-								<liferay-ui:custom-attribute-list
+								<liferay-custom-field-ui:custom-attribute-list
 									className="<%= DLFileEntryConstants.getClassName() %>"
 									classPK="<%= fileVersionId %>"
 									editable="<%= false %>"
 									label="<%= true %>"
 								/>
 							</liferay-ui:panel>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attribute-available>
 
 						<%
 						try {

modules/apps/collaboration/document-library/document-library-web/src/main/resources/META-INF/resources/image_gallery_display/view.jsp
@@ -197,14 +197,14 @@ List fileEntries = DLAppServiceUtil.getGroupFileEntries(scopeGroupId, 0, folderI
 						</div>
 					</div>
 
-					<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-available className="<%= DLFolderConstants.getClassName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFolderConstants.getClassName() %>"
 							classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 							editable="<%= false %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attribute-available>
 				</c:if>
 
 				<liferay-util:include page="/image_gallery_display/view_images.jsp" servletContext="<%= application %>" />

leonardol/com-liferay-document-library

document-library-web/src/main/resources/META-INF/resources/document_library/asset/folder_full_content.jsp
@@ -67,14 +67,14 @@ Folder folder = (Folder)request.getAttribute(WebKeys.DOCUMENT_LIBRARY_FOLDER);
 				</div>
 			</div>
 
-			<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= DLFolderConstants.getClassName() %>"
 					classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 		</aui:col>
 	</aui:row>
 </c:if>

document-library-web/src/main/resources/META-INF/resources/document_library/edit_file_entry.jsp
@@ -450,16 +450,16 @@ if (portletTitleBasedNavigation) {
 				</c:if>
 
 				<c:if test="<%= (folder == null) || folder.isSupportsMetadata() %>">
-					<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
+					<liferay-custom-field-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
 						<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-							<liferay-ui:custom-attribute-list
+							<liferay-custom-field-ui:custom-attribute-list
 								className="<%= DLFileEntryConstants.getClassName() %>"
 								classPK="<%= fileVersionId %>"
 								editable="<%= true %>"
 								label="<%= true %>"
 							/>
 						</aui:fieldset>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attributes-available>
 				</c:if>
 
 				<c:if test="<%= (folder == null) || folder.isSupportsSocial() %>">

document-library-web/src/main/resources/META-INF/resources/document_library/edit_folder.jsp
@@ -287,16 +287,16 @@ if (portletTitleBasedNavigation) {
 			</c:if>
 
 			<c:if test="<%= (parentFolder == null) || parentFolder.isSupportsMetadata() %>">
-				<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFolderConstants.getClassName() %>"
 							classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attributes-available>
 			</c:if>
 
 			<c:if test="<%= !rootFolder && (folder == null) %>">

document-library-web/src/main/resources/META-INF/resources/document_library/info_panel.jsp
@@ -248,14 +248,14 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(fileEntries) && ListUtil.isEmp
 						classPK="<%= fileEntry.getFileEntryId() %>"
 					/>
 
-					<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFileEntryConstants.getClassName() %>"
 							classPK="<%= fileVersion.getFileVersionId() %>"
 							editable="<%= false %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attributes-available>
 
 					<%
 					AssetEntry layoutAssetEntry = AssetEntryLocalServiceUtil.fetchEntry(DLFileEntryConstants.getClassName(), assetClassPK);

document-library-web/src/main/resources/META-INF/resources/document_library/upload_multiple_file_entries_resources.jsp
@@ -219,14 +219,14 @@ else {
 				</liferay-ui:panel>
 			</c:if>
 
-			<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= DLFileEntryConstants.getClassName() %>"
 					classPK="<%= fileVersionId %>"
 					editable="<%= true %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 		</c:if>
 
 		<c:if test="<%= (folder == null) || folder.isSupportsSocial() %>">

document-library-web/src/main/resources/META-INF/resources/document_library/view_file_entry.jsp
@@ -320,16 +320,16 @@ if (portletTitleBasedNavigation) {
 
 						</c:if>
 
-						<liferay-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>" classPK="<%= fileVersionId %>" editable="<%= false %>">
+						<liferay-custom-field-ui:custom-attributes-available className="<%= DLFileEntryConstants.getClassName() %>" classPK="<%= fileVersionId %>" editable="<%= false %>">
 							<liferay-ui:panel collapsible="<%= true %>" cssClass="lfr-custom-fields" id="documentLibraryCustomFieldsPanel" markupView="lexicon" persistState="<%= true %>" title="custom-fields">
-								<liferay-ui:custom-attribute-list
+								<liferay-custom-field-ui:custom-attribute-list
 									className="<%= DLFileEntryConstants.getClassName() %>"
 									classPK="<%= fileVersionId %>"
 									editable="<%= false %>"
 									label="<%= true %>"
 								/>
 							</liferay-ui:panel>
-						</liferay-ui:custom-attributes-available>
+						</liferay-custom-field-ui:custom-attributes-available>
 
 						<%
 						try {

document-library-web/src/main/resources/META-INF/resources/image_gallery_display/view.jsp
@@ -197,14 +197,14 @@ List fileEntries = DLAppServiceUtil.getGroupFileEntries(scopeGroupId, 0, folderI
 						</div>
 					</div>
 
-					<liferay-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
-						<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attributes-available className="<%= DLFolderConstants.getClassName() %>">
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= DLFolderConstants.getClassName() %>"
 							classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 							editable="<%= false %>"
 							label="<%= true %>"
 						/>
-					</liferay-ui:custom-attributes-available>
+					</liferay-custom-field-ui:custom-attributes-available>
 				</c:if>
 
 				<liferay-util:include page="/image_gallery_display/view_images.jsp" servletContext="<%= application %>" />
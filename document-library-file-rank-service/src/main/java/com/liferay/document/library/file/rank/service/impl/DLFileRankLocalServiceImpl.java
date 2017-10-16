jkappler/liferay-portal

LPS-74888 Adding custom methods from the original service

modules/apps/collaboration/document-library/document-library-filerank-service/src/main/java/com/liferay/document/library/filerank/service/impl/DLFileRankLocalServiceImpl.java
@@ -14,7 +14,24 @@
 
 package com.liferay.document.library.filerank.service.impl;
 
+import com.liferay.document.library.filerank.model.DLFileRank;
 import com.liferay.document.library.filerank.service.base.DLFileRankLocalServiceBaseImpl;
+import com.liferay.document.library.filerank.util.comparator.FileRankCreateDateComparator;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * The implementation of the document library file rank local service.
@@ -37,4 +54,211 @@ public class DLFileRankLocalServiceImpl extends DLFileRankLocalServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.document.library.filerank.service.DLFileRankLocalServiceUtil} to access the document library file rank local service.
 	 */
+	@Override
+	public DLFileRank addFileRank(
+		long groupId, long companyId, long userId, long fileEntryId,
+		ServiceContext serviceContext) {
+
+		long fileRankId = counterLocalService.increment();
+
+		DLFileRank dlFileRank = dlFileRankPersistence.create(fileRankId);
+
+		dlFileRank.setGroupId(groupId);
+		dlFileRank.setCompanyId(companyId);
+		dlFileRank.setUserId(userId);
+		dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
+		dlFileRank.setFileEntryId(fileEntryId);
+		dlFileRank.setActive(true);
+
+		try {
+			dlFileRankPersistence.update(dlFileRank);
+		}
+		catch (SystemException se) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Add failed, fetch {companyId=" + companyId + ", userId=" +
+						userId + ", fileEntryId=" + fileEntryId + "}");
+			}
+
+			dlFileRank = dlFileRankPersistence.fetchByC_U_F(
+				companyId, userId, fileEntryId, false);
+
+			if (dlFileRank == null) {
+				throw se;
+			}
+		}
+
+		return dlFileRank;
+	}
+
+	@Override
+	public void checkFileRanks() {
+		List<Object[]> staleFileRanks = dlFileRankFinder.findByStaleRanks(
+			PropsValues.DL_FILE_RANK_MAX_SIZE);
+
+		for (Object[] staleFileRank : staleFileRanks) {
+			long groupId = (Long)staleFileRank[0];
+			long userId = (Long)staleFileRank[1];
+
+			List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByG_U_A(
+				groupId, userId, true, PropsValues.DL_FILE_RANK_MAX_SIZE,
+				QueryUtil.ALL_POS, new FileRankCreateDateComparator());
+
+			for (DLFileRank dlFileRank : dlFileRanks) {
+				long fileRankId = dlFileRank.getFileRankId();
+
+				try {
+					dlFileRankPersistence.remove(dlFileRank);
+				}
+				catch (Exception e) {
+					if (_log.isWarnEnabled()) {
+						_log.warn("Unable to remove file rank " + fileRankId);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public void deleteFileRank(DLFileRank dlFileRank) {
+		dlFileRankPersistence.remove(dlFileRank);
+	}
+
+	@Override
+	public void deleteFileRank(long fileRankId) throws PortalException {
+		DLFileRank dlFileRank = dlFileRankPersistence.findByPrimaryKey(
+			fileRankId);
+
+		dlFileRankLocalService.deleteFileRank(dlFileRank);
+	}
+
+	@Override
+	public void deleteFileRanksByFileEntryId(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRankLocalService.deleteFileRank(dlFileRank);
+		}
+	}
+
+	@Override
+	public void deleteFileRanksByUserId(long userId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByUserId(
+			userId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRankLocalService.deleteFileRank(dlFileRank);
+		}
+	}
+
+	@Override
+	public void disableFileRanks(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(false);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	@Override
+	public void disableFileRanksByFolderId(long folderId)
+		throws PortalException {
+
+		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
+
+		updateFileRanks(dlFolder, false);
+	}
+
+	@Override
+	public void enableFileRanks(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(true);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	@Override
+	public void enableFileRanksByFolderId(long folderId)
+		throws PortalException {
+
+		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
+
+		updateFileRanks(dlFolder, true);
+	}
+
+	@Override
+	public List<DLFileRank> getFileRanks(long groupId, long userId) {
+		return dlFileRankPersistence.findByG_U_A(
+			groupId, userId, true, 0, PropsValues.DL_FILE_RANK_MAX_SIZE,
+			new FileRankCreateDateComparator());
+	}
+
+	@Override
+	public DLFileRank updateFileRank(
+		long groupId, long companyId, long userId, long fileEntryId,
+		ServiceContext serviceContext) {
+
+		if (!PropsValues.DL_FILE_RANK_ENABLED) {
+			return null;
+		}
+
+		DLFileRank dlFileRank = dlFileRankPersistence.fetchByC_U_F(
+			companyId, userId, fileEntryId);
+
+		if (dlFileRank != null) {
+			dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
+
+			try {
+				dlFileRankPersistence.update(dlFileRank);
+			}
+			catch (Exception e) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Update failed, fetch {companyId=" + companyId +
+							", userId=" + userId + ", fileEntryId=" +
+								fileEntryId + "}");
+				}
+			}
+		}
+		else {
+			dlFileRank = addFileRank(
+				groupId, companyId, userId, fileEntryId, serviceContext);
+		}
+
+		return dlFileRank;
+	}
+
+	protected void updateFileRanks(DLFolder dlFolder, boolean active) {
+		List<DLFolder> dlFolders = _dlFolderLocalService.getFolders(
+			dlFolder.getGroupId(), dlFolder.getFolderId());
+
+		for (DLFolder curDLFolder : dlFolders) {
+			updateFileRanks(curDLFolder, active);
+		}
+
+		List<DLFileRank> dlFileRanks = dlFileRankFinder.findByFolderId(
+			dlFolder.getFolderId());
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(active);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFileRankLocalServiceImpl.class);
+
+	@Reference
+	private DLFolderLocalService _dlFolderLocalService;
+
 }

leonardol/com-liferay-document-library

document-library-file-rank-service/src/main/java/com/liferay/document/library/file/rank/service/impl/DLFileRankLocalServiceImpl.java
@@ -14,7 +14,21 @@
 
 package com.liferay.document.library.file.rank.service.impl;
 
+import com.liferay.document.library.file.rank.model.DLFileRank;
 import com.liferay.document.library.file.rank.service.base.DLFileRankLocalServiceBaseImpl;
+import com.liferay.document.library.file.rank.util.comparator.FileRankCreateDateComparator;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.List;
 
 /**
  * The implementation of the document library file rank local service.
@@ -31,9 +45,214 @@ import com.liferay.document.library.file.rank.service.base.DLFileRankLocalServic
  * @see com.liferay.document.library.file.rank.service.DLFileRankLocalServiceUtil
  */
 public class DLFileRankLocalServiceImpl extends DLFileRankLocalServiceBaseImpl {
-	/*
+
+	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.document.library.file.rank.service.DLFileRankLocalServiceUtil} to access the document library file rank local service.
 	 */
+	@Override
+	public DLFileRank addFileRank(
+		long groupId, long companyId, long userId, long fileEntryId,
+		ServiceContext serviceContext) {
+
+		long fileRankId = counterLocalService.increment();
+
+		DLFileRank dlFileRank = dlFileRankPersistence.create(fileRankId);
+
+		dlFileRank.setGroupId(groupId);
+		dlFileRank.setCompanyId(companyId);
+		dlFileRank.setUserId(userId);
+		dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
+		dlFileRank.setFileEntryId(fileEntryId);
+		dlFileRank.setActive(true);
+
+		try {
+			dlFileRankPersistence.update(dlFileRank);
+		}
+		catch (SystemException se) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Add failed, fetch {companyId=" + companyId + ", userId=" +
+						userId + ", fileEntryId=" + fileEntryId + "}");
+			}
+
+			dlFileRank = dlFileRankPersistence.fetchByC_U_F(
+				companyId, userId, fileEntryId, false);
+
+			if (dlFileRank == null) {
+				throw se;
+			}
+		}
+
+		return dlFileRank;
+	}
+
+	@Override
+	public void checkFileRanks() {
+		List<Object[]> staleFileRanks = dlFileRankFinder.findByStaleRanks(
+			PropsValues.DL_FILE_RANK_MAX_SIZE);
+
+		for (Object[] staleFileRank : staleFileRanks) {
+			long groupId = (Long)staleFileRank[0];
+			long userId = (Long)staleFileRank[1];
+
+			List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByG_U_A(
+				groupId, userId, true, PropsValues.DL_FILE_RANK_MAX_SIZE,
+				QueryUtil.ALL_POS, new FileRankCreateDateComparator());
+
+			for (DLFileRank dlFileRank : dlFileRanks) {
+				long fileRankId = dlFileRank.getFileRankId();
+
+				try {
+					dlFileRankPersistence.remove(dlFileRank);
+				}
+				catch (Exception e) {
+					if (_log.isWarnEnabled()) {
+						_log.warn("Unable to remove file rank " + fileRankId);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public void deleteFileRank(DLFileRank dlFileRank) {
+		dlFileRankPersistence.remove(dlFileRank);
+	}
+
+	@Override
+	public void deleteFileRank(long fileRankId) throws PortalException {
+		DLFileRank dlFileRank = dlFileRankPersistence.findByPrimaryKey(
+			fileRankId);
+
+		dlFileRankLocalService.deleteFileRank(dlFileRank);
+	}
+
+	@Override
+	public void deleteFileRanksByFileEntryId(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRankLocalService.deleteFileRank(dlFileRank);
+		}
+	}
+
+	@Override
+	public void deleteFileRanksByUserId(long userId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByUserId(
+			userId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRankLocalService.deleteFileRank(dlFileRank);
+		}
+	}
+
+	@Override
+	public void disableFileRanks(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(false);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	@Override
+	public void disableFileRanksByFolderId(long folderId)
+		throws PortalException {
+
+		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
+
+		updateFileRanks(dlFolder, false);
+	}
+
+	@Override
+	public void enableFileRanks(long fileEntryId) {
+		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
+			fileEntryId);
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(true);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	@Override
+	public void enableFileRanksByFolderId(long folderId)
+		throws PortalException {
+
+		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
+
+		updateFileRanks(dlFolder, true);
+	}
+
+	@Override
+	public List<DLFileRank> getFileRanks(long groupId, long userId) {
+		return dlFileRankPersistence.findByG_U_A(
+			groupId, userId, true, 0, PropsValues.DL_FILE_RANK_MAX_SIZE,
+			new FileRankCreateDateComparator());
+	}
+
+	@Override
+	public DLFileRank updateFileRank(
+		long groupId, long companyId, long userId, long fileEntryId,
+		ServiceContext serviceContext) {
+
+		if (!PropsValues.DL_FILE_RANK_ENABLED) {
+			return null;
+		}
+
+		DLFileRank dlFileRank = dlFileRankPersistence.fetchByC_U_F(
+			companyId, userId, fileEntryId);
+
+		if (dlFileRank != null) {
+			dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
+
+			try {
+				dlFileRankPersistence.update(dlFileRank);
+			}
+			catch (Exception e) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Update failed, fetch {companyId=" + companyId +
+							", userId=" + userId + ", fileEntryId=" +
+								fileEntryId + "}");
+				}
+			}
+		}
+		else {
+			dlFileRank = addFileRank(
+				groupId, companyId, userId, fileEntryId, serviceContext);
+		}
+
+		return dlFileRank;
+	}
+
+	protected void updateFileRanks(DLFolder dlFolder, boolean active) {
+		List<DLFolder> dlFolders = dlFolderLocalService.getFolders(
+			dlFolder.getGroupId(), dlFolder.getFolderId());
+
+		for (DLFolder curDLFolder : dlFolders) {
+			updateFileRanks(curDLFolder, active);
+		}
+
+		List<DLFileRank> dlFileRanks = dlFileRankFinder.findByFolderId(
+			dlFolder.getFolderId());
+
+		for (DLFileRank dlFileRank : dlFileRanks) {
+			dlFileRank.setActive(active);
+
+			dlFileRankPersistence.update(dlFileRank);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFileRankLocalServiceImpl.class);
+
 }